bd40f2db4d4dd27224428ce750a8f3daa59c9d6e
semicolon jeff <palmbeachjeff@gmail.com>
2019-05-07 20:14:24 -0400

    cors error but first chart shows up

---
+++ charts.js
@@ -0,0 +1,11 @@
+
+$(document).ready(function () {
+  $('#container').highcharts({
+      title: {
+        text: 'Grand Canyon University Revenue'
+      },
+      data: {
+        googleSpreadsheetKey: '1eWmcnVnkgXo_nZ972vR_unWzVa0jt3C5TDwBhnxFMbM'
+      }
+  });
+});
